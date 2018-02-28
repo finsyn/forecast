90b778b4c9bff99c071d5e7d29366b3f6264bba6
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2018-02-28 21:38:10 +0100

    wip: real output

--- forecast.py
+++ forecast.py
@@ -14,16 +14,16 @@ from matplotlib import pyplot
 from sklearn import preprocessing
 from sklearn.metrics import average_precision_score
 
-dataset = read_csv('omx-v008.csv', header=0, index_col=0)
-data = get_train_data(dataset, target='omxState', n_lags=2)
+dataset = read_csv('omx-no-label.csv', header=0, index_col=0)
+data = get_train_data(dataset, target='omxPercentChange', n_lags=2)
 values = data.values
 
 # unique values in categorical output column
-state_to_idx = {state: idx for idx, state in enumerate(list(set(values[:,-1])))}
-n_output = len(state_to_idx)
+# state_to_idx = {state: idx for idx, state in enumerate(list(set(values[:,-1])))}
+# n_output = len(state_to_idx)
 
 # replace output column with index values
-values[:,-1] = [state_to_idx[x] for x in values[:,-1]]
+# values[:,-1] = [state_to_idx[x] for x in values[:,-1]]
 
 # normalize input
 # min_max_scaler = preprocessing.MinMaxScaler()
@@ -56,53 +56,40 @@ def omxmodel (n_inputs, n_features, n_values):
     # X = Dropout(0.5)(X)
     X = LSTM(64)(inputs)
     X = Dense(n_features)(X)
-    X = Dropout(0.8)(X)
-    predictions = Dense(n_values, activation='softmax')(X)
+    X = Dropout(0.2)(X)
+    predictions = Dense(n_values, activation='tanh')(X)
 
     model = Model(inputs=inputs, outputs=predictions)
 
     return model
 
-# onetrain hot encode target column
-train_y_oh = to_categorical(train_y, num_classes=n_output)
-test_y_oh  = to_categorical(test_y , num_classes=n_output)
-
-model = omxmodel(train_X.shape[1], train_X.shape[2], len(state_to_idx))
+model = omxmodel(train_X.shape[1], train_X.shape[2], 1)
 
 print(model.summary())
 
 # build network
 model.compile(
-        loss='categorical_crossentropy',
-        metrics=['accuracy'],
+        loss='mean_squared_error',
+        metrics=['mae'],
         optimizer='adam')
 
 # fit network
 history = model.fit(
-        train_X, train_y_oh,
+        train_X, train_y,
         epochs=200, batch_size=16,
-        validation_data=(test_X, test_y_oh),
+        validation_data=(test_X, test_y),
         shuffle=False)
 
 # performace metrics
-result = model.evaluate(test_X, test_y_oh, verbose=0)
-print('Test set accuracy: %s' % result[1])
+result = model.evaluate(test_X, test_y, verbose=0)
+print('Test set mean absolute error: %s' % result[1])
 
 test_output = model.predict(test_X)
-predictions = np.argmax(test_output, axis=-1) 
-correct_confident, predicted_confident = confident_precision(test_output, test_y)
-precision_avg = average_precision_score(test_y_oh, test_output)
-accuracy_confident = np.sum(correct_confident == predicted_confident)/correct_confident.shape[0]
-print(state_to_idx)
+predictions = test_output
+
 print('Test set all:')
 print(test_y)
-print(predictions)
-print('Test set confident:')
-print(correct_confident)
-print(predicted_confident)
-
-print('Test set average precision: %s'   % precision_avg) 
-print('Test set confident accuracy: %s' % accuracy_confident)
+print(predictions.reshape(len(test_y)))
 
 # save model
 model.save('model.h5')
