78d8dca30aa7395e083273a80d9eb2ba75bccff9
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2018-03-05 23:21:23 +0100

    fix: smarter short positions query

--- queries/shorts.sql
+++ queries/shorts.sql
@@ -1,10 +1,17 @@
 SELECT
   TIMESTAMP_TRUNC(published_at, DAY) AS date,
-  count(percent.float) AS sum
+# we do stupid aggregate over all companies per day now
+#  isin,
+  sum(positionDiff) as totalDiff
 FROM
-  `insikt-e1887.forecast.short_positions`
-WHERE comment = 'Fallit under 0,5%'
+  (
+  SELECT 
+    percent.float - lag(percent.float, 1, 0.0) over (partition by isin, position_holder_name order by published_at asc) as positionDiff,
+    *
+    FROM `insikt-e1887.forecast.short_positions`
+    )
 GROUP BY
-  date
+#  isin,
+  published_at
 ORDER BY
   date ASC
