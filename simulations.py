af22eb7522145cf8f72aab62c41ee91a5f9e2bcc
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2018-04-05 22:32:15 +0200

    chore: cleanup

--- simulations.py
+++
@@ -1,9 +0,0 @@
-# Simulate what an investment following the predictions would have resulted in
-
-def simulate (actual=[], predictions=[], amount=1000.0, threshold=0.002, leverage=1.0):
-
-    for i in range(len(predictions)):
-        if (predictions[i] > threshold):
-            amount = amount + amount * (leverage * actual[i])
-
-    return amount
