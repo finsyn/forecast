388e48573a7be68d1a7f9f4e65201342178bf3d8
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2018-05-10 17:25:02 +0200

    feat: IG backtesting

--- src/backtest.py
+++ src/backtest.py
@@ -4,8 +4,11 @@ from load import load_quotes_daily, read_data_csv
 from sweholidays import get_trading_close_holidays
 from matplotlib import pyplot
 
-leverage = 4
-cap = cap_safe = 10000
+leverage_long = 1
+leverage_ava = 8
+leverage_ig = 100
+
+cap_init = 10000
 
 res = np.genfromtxt('data/test-output.csv', delimiter=',')
 y_h = res[0]
@@ -16,6 +19,7 @@ df = read_data_csv('data/indexes.csv')
 
 o = pd.DataFrame()
 c = df.loc[df['id'] == 'market-index_OMX30']
+o['diff'] = c.c - c.o
 o['change'] = (c.c - c.o)/c.o
 
 # remove dates when STO is closed
@@ -31,19 +35,41 @@ o = o.reindex(index_range)
 o = o.interpolate(limit_direction='both')
 o = o.tail(n_lags)
 
-y = o.values
-y_h = y_h.astype(bool)
-cap_hist = []
-cap_hist_safe = []
+change_hist = o['change'].values
+diffs_hist = o['diff'].values
+pred_up_hist = y_h.astype(bool)
 
-for i in range(0, n_lags):
-    multiplier = leverage if y_h[i] else -1 * leverage
-    cap *= 1 + multiplier * y[i]
-    cap_safe *= 1 + leverage * y[i]
-    cap_hist.append(cap.copy())
-    cap_hist_safe.append(cap_safe.copy())
+def run_ava(cap_init, changes, predictions, leverage): 
+    cap_hist = []
+    cap = cap_init
+    for i in range(0, n_lags):
+        multiplier = leverage if predictions[i] else -1 * leverage
+        cap *= 1 + multiplier * changes[i] 
+        cap_hist.append(cap.copy())
+    return cap_hist
+
+def run_ig(cap_init, diffs, predictions, leverage): 
+    cap_hist = []
+    cap = cap_init
+    for i in range(0, n_lags):
+        multiplier = leverage if predictions[i] else -1 * leverage
+        cap += multiplier * diffs[i]
+        cap -= leverage * 0.5
+        cap_hist.append(cap.copy())
+    return cap_hist
 
 
-print(len(cap_hist))
-pyplot.plot(range(0, n_lags), cap_hist, cap_hist_safe)
+def run_long(cap_init, changes, leverage): 
+    cap_hist = []
+    cap = cap_init
+    for i in range(0, n_lags):
+        cap *= 1 + leverage * changes[i]
+        cap_hist.append(cap.copy())
+    return cap_hist
+
+cap_hist_safe = run_long(cap_init, change_hist, leverage_long)
+cap_hist_ava = run_ava(cap_init, change_hist, pred_up_hist, leverage_ava)
+cap_hist_ig = run_ig(cap_init, diffs_hist, pred_up_hist, leverage_ig)
+
+pyplot.plot(range(0, n_lags), cap_hist_ava, 'g-', cap_hist_safe, 'b-', cap_hist_ig, 'r-')
 pyplot.show()
