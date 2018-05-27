c07dd165dd03c2684998670a31378e437ee57ccf
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2018-05-27 22:13:36 +0200

    fix: get high and low from CFDs

--- queries/cfds.sql
+++ queries/cfds.sql
@@ -2,7 +2,9 @@ SELECT
   TIMESTAMP_TRUNC(openQuotes.date, DAY) AS date,
   openQuotes.service_id as id,
   closeQuotes.newestClose as c,
-  openQuotes.newestOpen as o
+  openQuotes.newestOpen as o,
+  max(extremeQuotes.newestHighest) as h,
+  min(extremeQuotes.newestLowest) as l
 FROM
 (
   SELECT
@@ -13,6 +15,7 @@ FROM
     WHERE format_timestamp("%H:%M", date, "Europe/Stockholm") = '09:00'
     AND service_id = 'cfd_OMX30-20SEK-HOUR'
 ) openQuotes
+
 INNER JOIN (
   SELECT
     # we might have duplicates, if so  want the latest added value
@@ -23,6 +26,19 @@ INNER JOIN (
     AND service_id = 'cfd_OMX30-20SEK-HOUR'
 ) closeQuotes
 on format_timestamp("%F", closeQuotes.date) = format_timestamp("%F", openQuotes.date)
+
+INNER JOIN (
+  SELECT
+  first_value(high) over (partition by service_id, date order by created_at desc) as newestHighest,
+  first_value(low) over (partition by service_id, date order by created_at desc) as newestLowest,
+  *
+  FROM `notifications.quotes`
+  WHERE format_timestamp("%H:%M", date, "Europe/Stockholm") >= '09:00'
+  AND   format_timestamp("%H:%M", date, "Europe/Stockholm") <= '17:00'
+  AND service_id = 'cfd_OMX30-20SEK-HOUR'
+) extremeQuotes
+on format_timestamp("%F", closeQuotes.date) = format_timestamp("%F", extremeQuotes.date)
+
 GROUP BY
   date,
   id,
