9c1ed4ccc650a76f45aceb45d7838b4be22b09ec
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2018-07-22 20:10:27 +0200

    ci: better gcloud structure

--- publish-model.sh
+++ publish-model.sh
@@ -3,4 +3,4 @@
 # uploads all trained models and necessary metadata so it can be
 # consumed by our CI pipeline
 
-gsutil cp outputs/* gs://finsyn-ml-data/outputs/$(date +%F)/
+gsutil cp outputs/* gs://finsyn-ml-data/$(date +%F)/outputs/
